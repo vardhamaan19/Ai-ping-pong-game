a8daa91be7c97d6f54cd366cb23c44ef75346923
vardhamaan19 <80203030+vardhamaan19@users.noreply.github.com>
2022-01-17 19:47:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -23,8 +23,18 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
-}
+    canvas.parent('canvas');
+    video = createCapture(VIDEO);
+      video.size(800,400);
+    video.parent('game_console');
+    poseNet = ml5.poseNet(video, modelLoaded);
+      poseNet.on('pose', gotPoses);
+  
+  }
 
+  function modelLoaded() {
+    console.log('Model Loaded!');
+    }
 
 function draw(){
 
